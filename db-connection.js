060af061332812b623abe03d635f8e28ab583899
knowOne08 <yashdarji23082004@gmail.com>
2023-02-07 12:12:21 +0530

    debugging

--- db-connection.js
+++ db-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 let uri = "mongodb+srv://usr-fccMongoDB:"+process.env.MONGO_PASS+"@fccmongodb.ywcwgtz.mongodb.net/?retryWrites=true&w=majority"
-const db =  mongoose.connect(process.env.MONGO_URI,{
+const db =  mongoose.connect(uri,{
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
--- routes/api.js
+++ routes/api.js
@@ -8,51 +8,52 @@ module.exports = function (app) {
   app.use(bodyParser.json()) 
   app.route('/api/issues/:project')
 
-    .get(function (req, res){
-      let projectName = req.params.project;
-      const {
-        _id,
-        open,
-        issue_title,
-        issue_text,
-        created_by,
-        assigned_to,
-        status_text
-      } = req.query;
+  .get(function (req, res) {
+    let projectName = req.params.project;
+    //?open=true&assigned_to=Joe
+    const {
+      _id,
+      open,
+      issue_title,
+      issue_text,
+      created_by,
+      assigned_to,
+      status_text,
+    } = req.query;
 
-      ProjectModel.aggregate([
-        { $match: {name: projectName}},
-        { $unwind: "$issues"},
-        _id != undefined
-          ? { $match: {"issue._id": ObjectId(_id)}}
-          : { $match: {}},
-        open != undefined
-          ? { $match: {"issue.open": open }}
-          : { $match: {}},
-        issue_title != undefined
-          ? { $match: {"issue.issue_title": issue_title }}
-          : { $match: {}},
-        issue_text != undefined
-          ? { $match: {"issue.issue_text": issue_text }}
-          : { $match: {}},
-        created_by != undefined
-          ? { $match: {"issue.created_by": created_by }}
-          : { $match: {}},
-        assigned_to != undefined
-          ? { $match: {"issue.assigned_to": assigned_to }}
-          : { $match: {}},
-        status_text != undefined
-          ? { $match: {"issue.status_text": status_text }}
-          : { $match: {}},
-      ]).exec((err,data)=>{
-        if(!data){
-          res.json([])
-        } else {
-          let mappedData = data.map((item)=>item.issues);
-          res.json(mappedData);
-        }
-      });
-    })
+    ProjectModel.aggregate([
+      { $match: { name: projectName } },
+      { $unwind: "$issues" },
+      _id != undefined
+        ? { $match: { "issues._id": ObjectId(_id) } }
+        : { $match: {} },
+      open != undefined
+        ? { $match: { "issues.open": open } }
+        : { $match: {} },
+      issue_title != undefined
+        ? { $match: { "issues.issue_title": issue_title } }
+        : { $match: {} },
+      issue_text != undefined
+        ? { $match: { "issues.issue_text": issue_text } }
+        : { $match: {} },
+      created_by != undefined
+        ? { $match: { "issues.created_by": created_by } }
+        : { $match: {} },
+      assigned_to != undefined
+        ? { $match: { "issues.assigned_to": assigned_to } }
+        : { $match: {} },
+      status_text != undefined
+        ? { $match: { "issues.status_text": status_text } }
+        : { $match: {} },
+    ]).exec((err, data) => {
+      if (!data) {
+        res.json([]);
+      } else {
+        let mappedData = data.map((item) => item.issues);
+        res.json(mappedData);
+      }
+    });
+  })
     
     .post(function (req, res) {
       let project = req.params.project;
@@ -82,7 +83,11 @@ module.exports = function (app) {
           const newProject = new ProjectModel({ name: project });
           newProject.issues.push(newIssue);
           newProject.save((err, data) => {
-           
+            if (err || !data) {
+              res.send("There was an error saving in post");
+            } else {
+              res.json(newIssue);
+            }
           });
         } else {
           projectdata.issues.push(newIssue);
@@ -97,59 +102,58 @@ module.exports = function (app) {
       });
     })
 
-    .put(function (req, res){
+    .put(function (req, res) {
       let project = req.params.project;
       const {
         _id,
-        open,
         issue_title,
         issue_text,
         created_by,
         assigned_to,
-        status_text
+        status_text,
+        open,
       } = req.body;
-      if(!_id){
-        res.json({error: "missing _id"});
+      if (!_id) {
+        res.json({ error: "missing _id" });
         return;
-
-      } 
+      }
       if (
         !issue_title &&
         !issue_text &&
         !created_by &&
         !assigned_to &&
         !status_text &&
-        !open 
-      ){
-        res.json({error: "no update field(s) sent", _id: _id});
+        !open
+      ) {
+        res.json({ error: "no update field(s) sent", _id: _id });
         return;
       }
-    ProjectModel.findOne({name: project}, (err,projectdata)=>{
-      if(err|| !projectdata){ 
-        res.json({error:"could not update", _id: _id});
-      } else {
-        const issueData = projectdata.issues.id(_id);
-        if(!issueData){
-          res.json({error: "could not update", _id: _id});
-          return;
-        }
-        issueData.issue_title = issue_title || issueData.issue_title;
-        issueData.issue_text = issue_text || issueData.issue_text;
-        issueData.created_by = created_by || issueData.created_by;
-        issueData.issue_title = assigned_to || issueData.assigned_to;
-        issueData.issue_title = status_text || issueData.status_text;
-        issueData.updated_on = new Date();
-        issueData.open = open;
-        projectdata.save((err,data)=>{
-          if(err || !data){
-            res.json({error:"could not update", _id: _id});
-          } else {
-            res.json({result:"successfully <updated></updated>", _id: _id});
 
+      ProjectModel.findOne({ name: project }, (err, projectdata) => {
+        if (err || !projectdata) {
+          res.json({ error: "could not update", _id: _id });
+        } else {
+          const issueData = projectdata.issues.id(_id);
+          if (!issueData) {
+            res.json({ error: "could not update", _id: _id });
+            return;
           }
-        })
-      }
-    })
+          issueData.issue_title = issue_title || issueData.issue_title;
+          issueData.issue_text = issue_text || issueData.issue_text;
+          issueData.created_by = created_by || issueData.created_by;
+          issueData.assigned_to = assigned_to || issueData.assigned_to;
+          issueData.status_text = status_text || issueData.status_text;
+          issueData.updated_on = new Date();
+          issueData.open = open;
+          projectdata.save((err, data) => {
+            if (err || !data) {
+              res.json({ error: "could not update", _id: _id });
+            } else {
+              res.json({ result: "successfully updated", _id: _id });
+            }
+          });
+        }
+      });
     })
     
     .delete(function (req, res){
